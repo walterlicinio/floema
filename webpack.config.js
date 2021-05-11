f56a3540aae5db55b135aaae373cd69ac92723e4
walterlicinio <licinio.laticinio@gmail.com>
2021-05-11 11:39:28 -0300

    �

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,23 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const IS_DEVELOPMENT = process.env.NODE_ENV==='dev'
 
 const dirApp = path.join(__dirname, 'app')
-const dirAssets = path.join(__dirname, 'assets')
+const dirShared = path.join(__dirname, 'assets')
 const dirStyles = path.join(__dirname, 'styles')
+const dirNode = 'node_modules'
+//console.log(dirApp, dirAssets, dirStyles)
 
-console.log(dirApp, dirAssets, dirStyles)
+module.exports = {
+  entry: [
+    path.join(dirApp,'index.js'),
+    path.join(dirStyles,'index.scss')
+  ],
+  resolve: {
+    modules: [dirApp, dirShared, dirStyles, dirNode],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT
+    }), 
+    new CopyWebpackPlugin
+  ]
+}
