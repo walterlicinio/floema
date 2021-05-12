fc0224c1cd1a1dd9ae0bb42a8b58327934057a43
walterlicinio <licinio.laticinio@gmail.com>
2021-05-12 01:03:45 -0300

    Complete Boilerplate

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -3,13 +3,13 @@ const path = require('path')
 
 const config = require('./webpack.config')
 
-module.exports = merge (config, {
+module.exports = merge(config, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     writeToDisk: true
   },
-  output :{
-    path: path.resolve(__dirname,'public')
+  output: {
+    path: path.resolve(__dirname, 'public')
   }
 })
